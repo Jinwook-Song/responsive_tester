c1efa0a014631926e7047c30fea21c9bb95c59eb
jinwook song <wlsdnr129@naver.com>
2022-05-29 04:17:04 +0900

    refactoring

--- main.py
+++ main.py
@@ -7,30 +7,44 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-# option to be kept open
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+class ResponsiveTester:
+    def __init__(self, urls):
+        self.chrome_options = Options()
+        self.driver = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()),
+        )
+        self.driver.maximize_window()
 
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()), options=chrome_options
-)
-driver.get("https://nomadcoders.co")
-driver.maximize_window()
+        self.urls = urls
+        # Check Lists
+        self.sizes = [480, 960, 1440]
+        # Device screen height 790
+        self.height = self.driver.get_window_size()["height"]
 
-# Check Lists
-sizes = [480, 960, 1440]
+    def screenshots(self, url):
+        self.driver.get(url)
+        for size in self.sizes:
+            self.driver.set_window_size(size, self.height)
+            self.driver.execute_script("window.scrollTo(0, 0)")
+            time.sleep(1.5)
+            # return JS -> Python
+            scroll_size = self.driver.execute_script(
+                "return document.body.scrollHeight"
+            )
+            total_sections = math.ceil(scroll_size / self.height)
+            for section in range(total_sections + 1):
+                self.driver.execute_script(
+                    f"window.scrollTo(0, {(section) * self.height})"
+                )
+                self.driver.save_screenshot(f"screenshots/{size}_{section}.png")
+                time.sleep(0.5)
 
-# Device screen height 790
-height = driver.get_window_size()["height"]
+    def start(self):
+        for url in self.urls:
+            self.screenshots(url)
 
-for size in sizes:
-    driver.set_window_size(size, height)
-    driver.execute_script("window.scrollTo(0, 0)")
-    time.sleep(1.5)
-    # return JS -> Python
-    scroll_size = driver.execute_script("return document.body.scrollHeight")
-    total_sections = math.ceil(scroll_size / height)
-    for section in range(total_sections + 1):
-        driver.execute_script(f"window.scrollTo(0, {(section) * height})")
-        driver.save_screenshot(f"screenshots/{size}_{section}.png")
-        time.sleep(0.5)
+        self.driver.quit()
+
+
+tester = ResponsiveTester(["https://nomadcoders.co"])
+tester.start()
